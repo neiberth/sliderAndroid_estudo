c0abba3548f9de4e25783a85ec88a5f9c833646b
neiberth <neiberth@gmail.com>
2020-03-30 11:28:31 -0300

    atualizado o projeto

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Slider Android - Estudo
 
-[![material-intro](https://github.com/heinrichreimer/material-intro)]
+
+Link para a biblioteca Slider no Android
+[material-intro](https://github.com/heinrichreimer/material-intro)
--- Slider/app/src/main/java/br/com/neiberth/slider/MainActivity.java
+++ Slider/app/src/main/java/br/com/neiberth/slider/MainActivity.java
@@ -1,14 +1,33 @@
 package br.com.neiberth.slider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .fragment(R.layout.slider1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .fragment(R.layout.slider2)
+                .build()
+        );
     }
 }
